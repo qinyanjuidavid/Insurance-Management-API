272127b3e8423f26f4c34b22a17302da743d518e
qinyanjuidavid <davidkinyanjui052@gmail.com>
2023-02-07 14:33:06 +0300

    :construction_worker: insurance product class

--- insurance-management/src/main/java/com/example/insurancemanagement/insurance/client.java
+++ insurance-management/src/main/java/com/example/insurancemanagement/insurance/client.java
@@ -14,7 +14,6 @@ public class client {
     private String beneficiariesPhone;
     private String beneficiariesEmail;
 
-    //constructor
     public client(  Long id,
                     String name,
                     String address,
@@ -39,7 +38,6 @@ public class client {
         this.beneficiariesEmail = beneficiariesEmail;
     }
 
-    //getters and setters
     public Long getId() {
         return id;
     }
@@ -128,13 +126,62 @@ public class client {
         this.beneficiariesEmail = beneficiariesEmail;
     }
 
-    //toString
+
     @Override
     public String toString() {
         return "client [address=" + address + ", beneficiaries=" + beneficiaries + ", beneficiariesEmail="
                 + beneficiariesEmail + ", beneficiariesPhone=" + beneficiariesPhone + ", email=" + email + ", id=" + id
                 + ", name=" + name + ", nextOfKin=" + nextOfKin + ", nextOfKinEmail=" + nextOfKinEmail
                 + ", nextOfKinPhone=" + nextOfKinPhone + ", phone=" + phone + "]";
+    }
+
+}
+
+
+public class Product{
+    private Long id;
+    private String product_name;
+    private String description;
+    private String product_type;
+    private double price;
+
+    public Product(Long id, String product_name, String description, String product_type, double price) {
+        this.id = id;
+        this.product_name = product_name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProduct_type() {
+        return product_type;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
 }
